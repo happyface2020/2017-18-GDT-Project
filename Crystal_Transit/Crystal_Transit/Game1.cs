e940d722a4b54b2bdfefeae03341369610c5cba9
happyface2020 <goodreads01@gmail.com>
2017-12-23 00:42:35 -0500

    Basic Code
    Made Class MapLoad and Tileset
    Tileset - calculated the tile
    MapLoad - loads in 1 of our 4 maps

---
+++ Crystal_Transit/Crystal_Transit/Game1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace Crystal_Transit
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        public static string mapOne;
+        public const int WindowWidth = 960;
+        public const int WindowHeight = 640;
+        public static string output = Path.Combine(Directory.GetCurrentDirectory(), "PlayerOutput.txt");
+        public const int SquaresDepth = 2;
+        public const int SquareWidth = 15;
+        public const int SquaresHeight = 10;
+        public const int Scale = 64;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = WindowWidth;
+            graphics.PreferredBackBufferHeight = WindowHeight;
+        }
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            TileSet.TileSetTexture = Content.Load<Texture2D>("TileSet");
+        }
+        protected override void UnloadContent()
+        {
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap);
+
+            for (int z = 0; z < SquaresDepth; z++)
+            {
+                int layer = z;
+                for (int y = 0; y < 50; y++)
+                {
+                    int row = y;
+                    for (int x = 0; x < 100; x++)
+                    {
+                        int column = x;
+                        // batch.Draw(SpriteTexture, position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+
+                        spriteBatch.Draw(
+                            TileSet.TileSetTexture,
+                            new Rectangle((x * Scale), (y * Scale), Scale, Scale),
+                            //Tile.GetSourceRectangle(myMap.Rows[y].Columns[x].TileID),
+                            //Color.White);
+                            TileSet.GetSourceRectangle(MapLoad.Maps(1,layer, row, column)),
+                            Color.White,
+                            0f,
+                            Vector2.Zero,
+                            SpriteEffects.None,
+                            0f);
+
+                    }
+                }
+            }
+            base.Draw(gameTime);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Crystal_Transit/Crystal_Transit/TileSet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Crystal_Transit
+{
+    static class TileSet
+    {
+        static public Texture2D TileSetTexture;
+        static public int TilesetScale = 16; //tileset, pixel by one tile
+
+        static public Rectangle GetSourceRectangle(int tileIndex)
+        {
+            int tileY = tileIndex / (TileSetTexture.Width / TilesetScale);
+            int tileX = tileIndex % (TileSetTexture.Width / TilesetScale);
+
+            return new Rectangle(tileX * TilesetScale, tileY * TilesetScale, TilesetScale, TilesetScale);
+        }
+
+
+    }
+}
