69a318b292d27b333f483be8a711ad7e5cdc6b7f
ajperry4218 <35818537+ajperry4218@users.noreply.github.com>
2018-02-01 16:20:52 -0500

    Add files via upload

---
+++ Crystal_Transit/Xbox Controller.cs
@@ -0,0 +1,25 @@
+	GamePadState gamePad State = GamePad.GetState (PlayerIndex.One);
+		if (gamePadState.IsConnected)
+		{
+			
+`		}
+		if(gamepadState.Buttons.X == ButtonState>Pressed
+		{
+		
+		}
+		float  maxSpeed = 0.1f;
+		float  changeInAngle = gamepadState.Thumbsticks.Left.X * max speed;
+		Angle += changeInAngle
+`		{
+		
+		}
+		float leftTriggerValue = gamePadState.Triggers.Left;
+		{
+		
+		}
+		GamePadState newstate =GamePad.GetState(PlayerIndex.One);
+		if(newState.Buttons.X == ButtonState.Pressed &&
+					oldState.Buttons.X == ButtonState.Released
+
+
+
