0ea9d8e5dd46924a5c5ddea7e2b19ecfec747d48
BalR01g <33636650+BalR01g@users.noreply.github.com>
2018-02-10 20:17:51 -0500

    Add files via upload

---
+++ Crystal_Transit/Crystal_Transit/ClassButton.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Crystal_Transit
+{
+    class ClassButton
+    {
+        Texture2D Rtexture;
+        Vector2 Rposition;
+        Rectangle Rrectangle;
+
+        Color Rcolour = new Color(100, 255, 100, 255);
+
+        public Vector2 Rsize;
+
+        public ClassButton(Texture2D newTexture, GraphicsDevice Rgraphics)
+        {
+            Rtexture = newTexture;
+
+            //ScreenW = 1000, ScreenH = 1000
+            //ImgW = 100, ImgH = 20
+            Rsize = new Vector2(Rgraphics.Viewport.Width / 8, Rgraphics.Viewport.Height / 30);
+        }
+
+        bool down;
+        public bool RisClicked;
+        public void Update(MouseState Rmouse)
+        {
+            Rrectangle = new Rectangle((int)Rposition.X, (int)Rposition.Y,
+                (int)Rsize.X, (int)Rsize.Y);
+            Rectangle mouseRectangle = new Rectangle(Rmouse.X, Rmouse.Y, 1, 1);
+
+            if (mouseRectangle.Intersects(Rrectangle))
+            {
+                if (Rcolour.A == 255) down = false;
+                if (Rcolour.A == 0) down = true;
+                if (down) Rcolour.A += 3; else Rcolour.A -= 3;
+                if (Rmouse.LeftButton == ButtonState.Pressed) RisClicked = true;
+
+            }
+            else if (Rcolour.A < 255)
+            {
+                Rcolour.A += 3;
+                RisClicked = false;
+            }
+        }
+
+        public void setPosition(Vector2 newPosition)
+        {
+            Rposition = newPosition;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Rtexture, Rrectangle, Rcolour);
+        }
+    }
+}
--- Crystal_Transit/Crystal_Transit/Game1.cs
+++ Crystal_Transit/Crystal_Transit/Game1.cs
@@ -8,6 +8,18 @@ namespace Crystal_Transit
 {
     public class Game1 : Game
     {
+
+        enum GameState
+        {
+            Game1,
+            Options,
+            Playing,
+        }
+
+        GameState CurrentGameState = GameState.Game1;
+
+        ClassButton btnPlay;
+
         #region variables
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
@@ -28,15 +40,11 @@ namespace Crystal_Transit
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            graphics.PreferredBackBufferWidth = WindowWidth; //set size of window
-            graphics.PreferredBackBufferHeight = WindowHeight;
-            IsMouseVisible = true;
             
         }
         protected override void Initialize()
         {
             TileSet.TileSetTexture = Content.Load<Texture2D>("TileSet"); // loading tileset
-
             hero.texture = Content.Load<Texture2D>("Hero"); //change later  
             hero.position = new Vector2(100, 100);
 
@@ -51,6 +59,12 @@ namespace Crystal_Transit
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            graphics.PreferredBackBufferWidth = WindowWidth; //set size of window
+            graphics.PreferredBackBufferHeight = WindowHeight;
+            graphics.ApplyChanges();
+            IsMouseVisible = true;
+            btnPlay = new ClassButton(Content.Load<Texture2D>("Start"), graphics.GraphicsDevice);
+            btnPlay.setPosition(new Vector2(350, 300));
         }
         protected override void UnloadContent()
         {
@@ -60,68 +74,94 @@ namespace Crystal_Transit
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            hero.Update(gameTime);
-            camera.Follow(hero);
-            if (hero.position.X < 0) //basic unloading
+            MouseState mouse = Mouse.GetState();
+
+            switch (CurrentGameState)
             {
-                UnloadContent();
-                map = 1;
-                hero.position.X = 64 * 74;
-                LoadContent();
+                case GameState.Game1:
+                    if (btnPlay.RisClicked == true) CurrentGameState = GameState.Playing;
+                    btnPlay.Update(mouse);
+                    break;
+                case GameState.Playing:
+                    hero.Update(gameTime);
+                    camera.Follow(hero);
+                    if (hero.position.X < 0) //basic unloading
+                    {
+                        UnloadContent();
+                        map = 1;
+                        hero.position.X = 64 * 74;
+                        LoadContent();
+                    }
+
+                    archer.Update(gameTime);
+                    archer.targetMovedTo(hero.position);
+
+                    
+                    break;
+                
             }
-
-            archer.Update(gameTime);
-            archer.targetMovedTo(hero.position);
-
             base.Update(gameTime);
         }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, null, null, null, transformMatrix: camera.Transfrom);
+            
 
-            for (int z = 0; z < SquaresDepth; z++)
+            switch (CurrentGameState)
             {
-                int layer = z;
-                for (int y = 0; y < 50; y++)
-                {
-                    int row = y;
-                    for (int x = 0; x < 100; x++)
+                case GameState.Game1:
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(Content.Load<Texture2D>("MainMenu"), new Rectangle(0, 0, WindowWidth, WindowHeight), Color.White);
+                    btnPlay.Draw(spriteBatch);
+                    break;
+                case GameState.Playing:
+                    spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointWrap, null, null, null, transformMatrix: camera.Transfrom);
+                    for (int z = 0; z < SquaresDepth; z++)
                     {
-                        int column = x;
+                        int layer = z;
+                        for (int y = 0; y < 50; y++)
+                        {
+                            int row = y;
+                            for (int x = 0; x < 100; x++)
+                            {
+                                int column = x;
 
-                        spriteBatch.Draw(
-                            TileSet.TileSetTexture,
-                            new Rectangle((x * Scale), (y * Scale), Scale, Scale),
-                            TileSet.GetSourceRectangle(MapLoad.Maps(map, layer, row, column)), //need to make variable fro map
-                            Color.White,
-                            0f,
-                            Vector2.Zero,
-                            SpriteEffects.None,
-                            0f);
+                                spriteBatch.Draw(
+                                    TileSet.TileSetTexture,
+                                    new Rectangle((x * Scale), (y * Scale), Scale, Scale),
+                                    TileSet.GetSourceRectangle(MapLoad.Maps(map, layer, row, column)), //need to make variable fro map
+                                    Color.White,
+                                    0f,
+                                    Vector2.Zero,
+                                    SpriteEffects.None,
+                                    0f);
 
+                            }
+                        }
                     }
-                }
+
+                    hero.Draw(spriteBatch);
+                    archer.Draw(spriteBatch);
+
+                    int tileNumNW = MapLoad.Maps(map, 1, (int)(hero.position.Y / Scale), (int)(hero.position.X / Scale));
+                    int tileNumSW = MapLoad.Maps(map, 1, (int)((hero.position.Y + hero.texture.Height * 3) / Scale), (int)(hero.position.X / Scale));
+                    int tileNumNE = MapLoad.Maps(map, 1, (int)(hero.position.Y / Scale), (int)((hero.position.X + hero.texture.Width * 3) / Scale));
+                    int tileNumSE = MapLoad.Maps(map, 1, (int)((hero.position.Y + hero.texture.Height * 3) / Scale), (int)((hero.position.X + hero.texture.Width * 3) / Scale));
+
+                    //((int)(hero.position.X/Scale)).ToString() + "," + ((int)(hero.position.Y / Scale)).ToString()  
+
+                    spriteBatch.DrawString(font, tileNumNW.ToString(), hero.position, Color.Black);
+                    spriteBatch.DrawString(font, tileNumSW.ToString(), new Vector2(hero.position.X, hero.position.Y + hero.texture.Height * 3), Color.Black);
+                    spriteBatch.DrawString(font, tileNumNE.ToString(), new Vector2(hero.position.X + hero.texture.Width * 3, hero.position.Y), Color.Black);
+                    spriteBatch.DrawString(font, tileNumSE.ToString(), new Vector2(hero.position.X + hero.texture.Width * 3, hero.position.Y + hero.texture.Height * 3), Color.Black);
+
+
+                    break;
+                
+                
             }
-
-            hero.Draw(spriteBatch);
-            archer.Draw(spriteBatch);
-
-            int tileNumNW = MapLoad.Maps(map, 1, (int)(hero.position.Y / Scale), (int)(hero.position.X / Scale));
-            int tileNumSW = MapLoad.Maps(map, 1, (int)((hero.position.Y + hero.texture.Height * 3) / Scale), (int)(hero.position.X / Scale));
-            int tileNumNE = MapLoad.Maps(map, 1, (int)(hero.position.Y / Scale), (int)((hero.position.X + hero.texture.Width * 3) / Scale));
-            int tileNumSE = MapLoad.Maps(map, 1, (int)((hero.position.Y + hero.texture.Height * 3) / Scale), (int)((hero.position.X + hero.texture.Width * 3) / Scale));
-
-            //((int)(hero.position.X/Scale)).ToString() + "," + ((int)(hero.position.Y / Scale)).ToString()  
-
-            spriteBatch.DrawString(font, tileNumNW.ToString(), hero.position, Color.Black);
-            spriteBatch.DrawString(font, tileNumSW.ToString(), new Vector2(hero.position.X, hero.position.Y + hero.texture.Height * 3), Color.Black);
-            spriteBatch.DrawString(font, tileNumNE.ToString(), new Vector2(hero.position.X + hero.texture.Width * 3, hero.position.Y), Color.Black);
-            spriteBatch.DrawString(font, tileNumSE.ToString(), new Vector2(hero.position.X + hero.texture.Width * 3, hero.position.Y + hero.texture.Height * 3), Color.Black);
-
-
-            base.Draw(gameTime);
             spriteBatch.End();
+            base.Draw(gameTime);
         }
     }
 }
