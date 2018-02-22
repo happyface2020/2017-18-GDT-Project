2635d42913cb34716f07bfc659959133e9407da1
frootluips <36054579+frootluips@users.noreply.github.com>
2018-02-22 16:52:41 -0500

    Update Inventory.cs

--- Crystal_Transit/Crystal_Transit/Inventory.cs
+++ Crystal_Transit/Crystal_Transit/Inventory.cs
@@ -8,5 +8,18 @@ namespace Crystal_Transit
 {
     class Inventory
     {
+        public Texture2D texture;
+        public Vector2 position;
+        public const float speed = 2f; //can change speed; set speed 
+
+        public virtual void Update(GameTime gameTime) //virtual can be overridden by any inherted class
+        {
+            
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, 0f, Vector2.Zero, 3, SpriteEffects.None, 0f); //CHANGES THE SCALE OF THE HERO
+        }
     }
 }
